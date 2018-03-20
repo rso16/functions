cfbfd4aeb4cb0cd3051116fcb91dfbca70cbd495
Rick Overhorst <s1119@tosec.nl>
2018-03-20 11:51:06 +0100

    fixed typo

--- functions.cpp
+++ functions.cpp
@@ -1,6 +1,6 @@
 #include "functions.h"
 
-uint8_t mergeNibles(uint8_t nible0, uint8_t nible1)
+uint8_t mergeNibbles(uint8_t nibble0, uint8_t nibble1)
 {
   uint8_t merged = (nible0 << 4) | nible1;
   return merged;
@@ -12,7 +12,7 @@ uint8_t ASCIIToHEX(uint8_t ASCIIVal)
   uint8_t hexval = 0;
   if( ASCIIVal >= '0' && ASCIIVal <= '9')
   {
-    hexval = ASCIIVal - '0';
+    hexval = ASCIIVal - ('0');
   }
   else if( ASCIIVal >= 'A' && ASCIIVal <= 'F')
   {
--- functions.h
+++ functions.h
@@ -1,4 +1,4 @@
 #include <stdint.h>
 
-uint8_t mergeNibles(uint8_t nible0, uint8_t nible1);
+uint8_t mergeNibbles(uint8_t nibble0, uint8_t nibble1);
 uint8_t ASCIIToHEX(uint8_t ASCIIVal);
