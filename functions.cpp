eb4335a10214da99a5c7d7c4e1c3d7479d784bfb
rso16 <rick.overhorst@gmail.com>
2018-04-23 13:46:12 +0200

    add functions arrayToByte32 and byte32ToArray

--- functions.cpp
+++ functions.cpp
@@ -20,3 +20,35 @@ uint8_t ASCIIToHEX(uint8_t ASCIIVal)
   }
   return hexval;
 }
+
+uint8_t HEXToASCII(uint8_t hexVal)
+{
+  uint8_t ASCIIVal = 0;
+  if( hexVal >= '0' && hexVal <= '9')
+  {
+    ASCIIVal = hexVal  + ('0');
+  }
+  else if( hexVal >= 'A' && hexVal <= 'F')
+  {
+      ASCIIVal = hexVal +  0x37;
+  }
+  return ASCIIVal;
+}
+
+uint32_t arrayToByte32(uint8_t byteArray[])
+{
+  uint32_t byte32 = 0x0;
+  byte32 =  (((uint32_t)   byteArray[0] << 24));
+  byte32 |= (((uint32_t) byteArray[1] << 16));
+  byte32 |= (((uint32_t) byteArray[2] << 8));
+  byte32 |= (((uint32_t) byteArray[3]));
+  return byte32;
+}
+
+void  byte32toArray(uint32_t byte32, uint8_t byteArray[])
+{
+  byteArray[0] = (uint8_t) (byte32 >> 24);
+  byteArray[1] = (uint8_t) ((byte32 << 8) >> 24);
+  byteArray[2] = (uint8_t) ((byte32 << 16) >> 24);
+  byteArray[3] = (uint8_t) ((byte32 << 24) >> 24);
+}
--- functions.h
+++ functions.h
@@ -1,4 +1,10 @@
 #include <stdint.h>
+#include <Arduino.h>
+#define ARRAY_SIZE 4
+
 
 uint8_t mergeNibbles(uint8_t nibble0, uint8_t nibble1);
 uint8_t ASCIIToHEX(uint8_t ASCIIVal);
+uint8_t HEXToASCII(uint8_t hexVal);
+uint32_t arrayToByte32(uint8_t byteArray[]);
+void  byte32toArray(uint32_t byte32, uint8_t byteArray[]);
